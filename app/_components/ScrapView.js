6fc591bb70df34fe79b5e54468b6f84cba7537ce
chaeniverse <chaehyun3253@gmail.com>
2026-07-04 21:32:32 +0900

    publish: 주식 리포트 반영 (2026-07-04 21:32)

--- app/_components/ScrapView.js
+++ app/_components/ScrapView.js
@@ -1,18 +1,8 @@
-// 하루치 스크랩 렌더 (추적 직무 칩 + 카테고리별 카드)
+// 하루치 스크랩 렌더 (카테고리별 카드)
 export default function ScrapView({ data }) {
-  const jobs = data?.tracked_jobs || [];
   const sections = data?.sections || [];
   return (
     <div className="scrap">
-      {jobs.length > 0 && (
-        <div className="scrap-jobs">
-          {jobs.map((j, i) => (
-            <span className="scrap-chip" key={i}>
-              {j.company} · {j.role}
-            </span>
-          ))}
-        </div>
-      )}
       {sections.map((sec, si) => (
         <div key={si}>
           <div className="scrap-section-title">{sec.category}</div>
